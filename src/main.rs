b009e07a2d04b260368713cfebca9834b51f10eb
Jonatha Richard <jonatharichard48@gmail.com>
2025-04-04 21:14:49 -0300

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod pilha;
+mod fila;
+mod lista;
+mod tabela_hash;
+mod arvore;
+
+fn main() {
+    println!("Projeto de Estruturas de Dados em Rust");
+    
+    // Aqui você pode chamar funções de teste de cada módulo se quiser
+    // Exemplo:
+    // pilha::teste_pilha();
+    // fila::teste_fila();
+}
+
