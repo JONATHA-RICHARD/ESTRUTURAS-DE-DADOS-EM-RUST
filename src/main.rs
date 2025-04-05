4a7f5395dc80d4cc96e2d9d111282b1d3ed26cc1
Jonatha Richard <jonatharichard48@gmail.com>
2025-04-04 21:29:08 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+mod lista_encadeada;
 mod pilha;
 mod fila;
-mod lista;
 mod tabela_hash;
-mod arvore;
+mod arvore_binaria;
 
 fn main() {
     println!("Projeto de Estruturas de Dados em Rust");
-    
-    // Aqui você pode chamar funções de teste de cada módulo se quiser
-    // Exemplo:
-    // pilha::teste_pilha();
-    // fila::teste_fila();
+
+    // Aqui você pode chamar funções específicas de teste se quiser, como:
+    // lista_encadeada::teste();
+    // pilha::teste();
+    // fila::teste();
+    // tabela_hash::teste();
+    // arvore_binaria::teste();
 }
+
 
